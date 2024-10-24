d227b5e02cf5995734b16f2e305f9c72aaa5e014
Danilo Ruziska <danilo.ruziska@visma.com>
2024-10-24 14:45:19 +0100

    initial commit

---
+++ DeprecatedProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DeprecatedProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
